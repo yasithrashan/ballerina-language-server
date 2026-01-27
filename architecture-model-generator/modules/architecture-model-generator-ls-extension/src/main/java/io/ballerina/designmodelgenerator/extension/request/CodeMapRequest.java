2b7924c16be99af2ab65198bb255eda104841f6a
Yasith Rashan <y.rashan22@gmail.com>
2026-01-27 09:08:38 +0530

    Update copyright year and @since tags, fix test paths

--- architecture-model-generator/modules/architecture-model-generator-ls-extension/src/main/java/io/ballerina/designmodelgenerator/extension/request/CodeMapRequest.java
+++ architecture-model-generator/modules/architecture-model-generator-ls-extension/src/main/java/io/ballerina/designmodelgenerator/extension/request/CodeMapRequest.java
@@ -1,5 +1,5 @@
 /*
- *  Copyright (c) 2025, WSO2 LLC. (http://www.wso2.com)
+ *  Copyright (c) 2026, WSO2 LLC. (http://www.wso2.com)
  *
  *  WSO2 LLC. licenses this file to you under the Apache License,
  *  Version 2.0 (the "License"); you may not use this file except
@@ -23,7 +23,7 @@ package io.ballerina.designmodelgenerator.extension.request;
  *
  * @param projectPath The path to the project for which code map is requested
  * @param changesOnly If true, returns code map only for changed files since last request
- * @since 1.0.0
+ * @since 1.6.0
  */
 public record CodeMapRequest(String projectPath, boolean changesOnly) {
 }
